485b16ced5e535e34f3f850350619efadc9c180c
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-01 13:36:53 -0700

    search functionality finished

--- src/Components/RecipeList.js
+++ src/Components/RecipeList.js
@@ -1,14 +1,24 @@
 
+import { useState } from "react"
 import ImageCard from "./ImageCard"
+import Search from "./Search"
+
 
 export default function RecipeList({recipes}) {
 
+    const[searchString, setSearch] = useState("")
 
+    function handleSearch(newSearch){
+      setSearch(newSearch)
+    }
 
-    const renderThumbnails = recipes.map(recipe=> <ImageCard key={recipe.id} name={recipe.name} image={recipe.image} />)
+    const filteredRecipes = recipes.filter(recipe => recipe.ingredients.some(ingredient => ingredient.toLowerCase().includes(searchString.toLowerCase()) ))
+
+    const renderThumbnails = filteredRecipes.map(recipe=> <ImageCard key={recipe.id} name={recipe.name} image={recipe.image} ingredients={recipe.ingredients} />)
 
     return (
         <div className="cards">
+            <Search handleSearch={handleSearch}/>
             {renderThumbnails}
         </div>
     )
