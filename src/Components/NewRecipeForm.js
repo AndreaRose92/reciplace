cc86e2826bfa761d3b88346d3b46c396fdb2ae6f
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-01 13:54:46 -0700

    started on new recipe form

--- src/Components/NewRecipeForm.js
+++ src/Components/NewRecipeForm.js
@@ -4,6 +4,17 @@
 
 export default function NewRecipeForm() {
     return (
-        <div>This'll be a form.</div>
+        <div>
+            <form>
+                <input type="text" name="title" placeholder="Recipe Title" /><br/>
+                <select name="meal">
+                    <option name="breakfast">Breakfast</option>
+                    <option name="lunch">Lunch</option>
+                    <option name="dinner">Dinner</option>
+                    <option name="dessert">Dessert</option>
+                </select><br/>
+                <input type="text" name="ingredient">Ingredients</input>
+            </form>
+        </div>
     )
 }
