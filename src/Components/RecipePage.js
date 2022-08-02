9920be2343318907cae92c698a073ab1540ae58d
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-02 14:13:38 -0700

    add ingredients

--- src/Components/RecipePage.js
+++ src/Components/RecipePage.js
@@ -7,9 +7,14 @@ export default function RecipePage({recipes}) {
     const params = useParams()
     console.log(params)
 
+    const recipe = recipes[params.recipeID]
+
+    const renderIngredients = recipe.ingredients.map(ingredient => <p>{ingredient}</p>)
+
     return (
         <div>
-            <h3>{recipes[params.recipeID].name}</h3>
+            <h3>{recipe.name}</h3>
+            <ul>{renderIngredients}</ul>
         </div>
     )
 }
