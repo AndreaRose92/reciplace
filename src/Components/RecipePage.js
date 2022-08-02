89843ec137b2d0c567ad0d4696374d7290e00350
Josephine Luu <josephinegluu@gmail.com>
2022-08-02 16:19:18 -0500

    directions and image added

--- src/Components/RecipePage.js
+++ src/Components/RecipePage.js
@@ -10,11 +10,15 @@ export default function RecipePage({recipes}) {
     const recipe = recipes[params.recipeID]
 
     const renderIngredients = recipe.ingredients.map(ingredient => <p>{ingredient}</p>)
+    const renderDirections = recipe.directions.map(directions => <p>{directions}</p>)
 
     return (
         <div>
             <h3>{recipe.name}</h3>
+            <img src={recipe.image} alt={recipe.name}></img>
             <ul>{renderIngredients}</ul>
+            <ul>{renderDirections}</ul>
+
         </div>
     )
 }
