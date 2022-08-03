9b04d46b9262aa15fa545d4e262723c1c19c536d
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-03 14:49:43 -0700

    'cleaned up code spacing'

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,6 @@ import { NavLink } from "react-router-dom";
 
 const linkStyles = {
     display: "inline-block",
-    // width: "50px",
     padding: "12px",
     margin: "0 6px 6px",
     background: "orange",
