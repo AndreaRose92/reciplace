ab64fbc9c28e320841ef56d2f12832918f8c524b
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-03 08:56:42 -0700

    styled recipe pages

--- src/Components/RecipePage.js
+++ src/Components/RecipePage.js
@@ -10,13 +10,21 @@ export default function RecipePage({recipes}) {
     const renderDirections = recipe.directions.map(directions => <li key={Math.random()} >{directions}</li>)
 
     return (
-        <div classname="recipePage">
-            <h2>{recipe.name}</h2>
-            <img src={recipe.image} alt={recipe.name}></img>
-            <h3>Ingredients</h3>
-            <ul>{renderIngredients}</ul>
-            <h3>Directions</h3>
-            <ol>{renderDirections}</ol>
+        <div>
+            <div className="recipePage">
+                <h2>{recipe.name}</h2>
+                <img className="fullImage" src={recipe.image} alt={recipe.name}></img>
+                <div className="listContainer">
+                    <div className="ingredientList">
+                        <h3>Ingredients</h3>
+                        <ul>{renderIngredients}</ul>
+                    </div>
+                    <div className="directionList">
+                        <h3>Directions</h3>
+                        <ol>{renderDirections}</ol>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
