f3fd3ac640da7102e719c2e1a5a46fb52de918e5
AndreaRose92 <104471962+AndreaRose92@users.noreply.github.com>
2022-08-04 14:01:10 -0700

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ export default function About() {
         <div className="about">
             <h4>Reciplace, an Online Place for Recipes</h4>
             <p>Welcome to Reciplace!</p>
-            <p>Here you can browse and share a collection of recipes, created by users like you.</p>
+            <p>Here you can browse and share our collection of recipes, created by users like you.</p>
             <small>Created by Andrea R. and Josephine L., for Flatiron School</small>
         </div>
     )
